0c6919fd6cf0b187980c0b628d23cc949f6ebe64
fkfkfk112233 <kk60607kk@gmail.com>
2026-09-04 15:22:11 +0800

    Update keyboardInput.js

--- js/input/keyboardInput.js
+++ js/input/keyboardInput.js
@@ -1,23 +1,23 @@
 import { changeDirection } from "./directionController.js";
 
-export function initKeyboardInput() {
-    document.addEventListener("keydown", handleKeyDown);
+export function initializeKeyboardInput() {
+  document.addEventListener("keydown", handleKeyDown);
 }
 
 function handleKeyDown(event) {
-    if (event.key === "ArrowUp") {
-        changeDirection("UP");
-    }
+  if (event.key === "ArrowUp") {
+    changeDirection("UP");
+  }
 
-    if (event.key === "ArrowDown") {
-        changeDirection("DOWN");
-    }
+  if (event.key === "ArrowDown") {
+    changeDirection("DOWN");
+  }
 
-    if (event.key === "ArrowLeft") {
-        changeDirection("LEFT");
-    }
+  if (event.key === "ArrowLeft") {
+    changeDirection("LEFT");
+  }
 
-    if (event.key === "ArrowRight") {
-        changeDirection("RIGHT");
-    }
+  if (event.key === "ArrowRight") {
+    changeDirection("RIGHT");
+  }
 }
