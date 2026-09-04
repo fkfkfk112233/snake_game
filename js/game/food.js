29e786d2e310e8ab34ed9855cc2e864b346eb254
fkfkfk112233 <kk60607kk@gmail.com>
2026-09-04 15:20:39 +0800

    Update food.js

--- js/game/food.js
+++ js/game/food.js
@@ -1,21 +1,27 @@
 import { game } from "../state/gameState.js";
 
-export function createFood() {
-  let newFood;
-
-  do {
-    newFood = {
-      x: Math.floor(Math.random() * game.boardSize),
-
-      y: Math.floor(Math.random() * game.boardSize),
-    };
-  } while (isOnSnake(newFood));
-
-  game.food = newFood;
-}
-
 function isOnSnake(position) {
   return game.snake.some(
-    (part) => part.x === position.x && part.y === position.y,
+    (segment) =>
+      segment.x === position.x &&
+      segment.y === position.y
   );
+}
+
+export function createFood() {
+  const maxAttempts = game.boardSize * game.boardSize;
+
+  for (let i = 0; i < maxAttempts; i++) {
+    const newFood = {
+      x: Math.floor(Math.random() * game.boardSize),
+      y: Math.floor(Math.random() * game.boardSize),
+    };
+
+    if (!isOnSnake(newFood)) {
+      game.food = newFood;
+      return true;
+    }
+  }
+
+  return false;
 }
