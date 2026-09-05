d4317f9926a26c77dcfbaa1c402194b14ab3968e
fkfkfk112233 <kk60607kk@gmail.com>
2026-09-05 20:41:36 +0800

    Create orientationController.js

---
+++ js/input/orientationController.js
@@ -0,0 +1,27 @@
+import { game, GAME_ORIENTATIONS } from "../state/gameState.js";
+import { DEVICE_TYPES } from "../device/deviceDetector.js";
+
+export async function lockGameOrientation() {
+  // Desktop 不需要處理螢幕方向
+  if (game.deviceType !== DEVICE_TYPES.MOBILE) {
+    return;
+  }
+
+  // 瀏覽器不支援 Screen Orientation API
+  if (!screen.orientation || !screen.orientation.lock) {
+    return;
+  }
+
+  try {
+    if (game.orientation === GAME_ORIENTATIONS.PORTRAIT) {
+      await screen.orientation.lock("portrait");
+      return;
+    }
+
+    if (game.orientation === GAME_ORIENTATIONS.LANDSCAPE) {
+      await screen.orientation.lock("landscape");
+    }
+  } catch (error) {
+    console.warn("Unable to lock screen orientation:", error);
+  }
+}
